aaaf4b907e224751f8f5da920a04c16fa2f94e0e
mac <mac@blockapi.dev>
2024-03-15 03:48:44 +0800

    hati-example init

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/MiviaLabs/hati"
+	"github.com/MiviaLabs/hati/core"
+	"github.com/MiviaLabs/hati/log"
+	"github.com/MiviaLabs/hati/module"
+	"github.com/MiviaLabs/hati/transport"
+)
+
+func main() {
+	hati := hati.New(core.Config{
+		Name: "test-app",
+		Transport: transport.TransportManagerConfig{
+			Redis: transport.RedisConfig{
+				On:       true,
+				Host:     "localhost",
+				Port:     "6379",
+				Username: "",
+				Password: "",
+				Database: 0,
+			},
+		},
+	})
+
+	m := module.New("test")
+
+	hati.AddModule(m)
+
+	if err := hati.Start(); err != nil {
+		panic(err)
+	}
+
+	var osSignal chan os.Signal = make(chan os.Signal, 1)
+	signal.Notify(osSignal, os.Interrupt, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-osSignal:
+			log.Warning("shutting down, please wait")
+
+			if err := hati.Stop(); err != nil {
+				panic(err)
+			}
+
+			os.Exit(0)
+		}
+	}
+}
---
+++ modules/test_module.go
@@ -0,0 +1,50 @@
+package modules
+
+// type TestModule struct {
+// 	core.Module
+// }
+
+// // // BeforeStart implements core.Module.
+// // func (tm TestModule) BeforeStart() {
+// // 	panic("unimplemented")
+// // }
+
+// // // BeforeStop implements core.Module.
+// // func (tm TestModule) BeforeStop() {
+// // 	panic("unimplemented")
+// // }
+
+// // // Start implements core.Module.
+// // func (tm TestModule) Start() {
+// // 	panic("unimplemented")
+// // }
+
+// // // Stop implements core.Module.
+// //
+// //	func (tm TestModule) Stop() {
+// //		panic("unimplemented")
+// //	}
+// func (tm *TestModule) Name() string {
+// 	return tm.Module.Name
+// }
+
+// func (tm *TestModule) BeforeStop() error {
+// 	fmt.Println("BeforeStop")
+
+// 	return nil
+// }
+
+// func (tm *TestModule) Start() error {
+// 	fmt.Println("Start")
+// 	return nil
+// }
+
+// func (tm *TestModule) Stop() error {
+// 	fmt.Println("Stop")
+// 	return nil
+// }
+
+// func (tm *TestModule) BeforeStart() error {
+// 	fmt.Println("BeforeStart")
+// 	return nil
+// }
